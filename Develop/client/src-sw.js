0788de16e750f9e827cc83400e496318463c3755
Feleg Teklezghi <felegteklezghi@gmail.com>
2024-11-12 01:06:56 -0500

    implemented asset caching

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -26,5 +26,23 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// TODO: Implement asset caching/
-registerRoute();
+// TODO: Implement asset caching
+
+const assetCache = new CacheFirst({
+  cacheName: "asset-cache",
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
+
+registerRoute(
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  assetCache
+);
